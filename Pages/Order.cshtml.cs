c591a0e62bfa6a9ed2106e4bdc034b9adf5c35fd
Neko <456852ss00@gmail.com>
2022-04-21 10:23:32 +0800

    add net6sql

--- Pages/Order.cshtml.cs
+++ Pages/Order.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Pizza.Model;
 using Pizza.Data;
 
@@ -17,7 +18,9 @@ namespace Pizza.Pages
 
         public void OnGet()
         {
-            PizzaOrders = _context.PizzzaOrders.ToList();
+            var a = _context.PizzzaOrders.FromSqlRaw("SELECT * FROM dbo.PizzzaOrders").Take(2);
+            PizzaOrders = a.ToList();
+            //PizzaOrders = _context.PizzzaOrders.ToList();
         }
     }
 }
